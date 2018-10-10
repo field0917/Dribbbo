da947f21a89c8c414cd670b4b44c45562843ed67
Ye <field0917@gmail.com>
2018-10-10 11:42:27 -0700

    Add overflow menu to shotDetail Activity with download, info, open photo url function

--- app/src/main/java/com/jiuzhang/yeyuan/dribbbo/shot_detail/ShotDetailActivity.java
+++ app/src/main/java/com/jiuzhang/yeyuan/dribbbo/shot_detail/ShotDetailActivity.java
@@ -4,7 +4,12 @@ import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.widget.Toast;
 
 import com.google.gson.reflect.TypeToken;
 import com.jiuzhang.yeyuan.dribbbo.R;
@@ -13,18 +18,36 @@ import com.jiuzhang.yeyuan.dribbbo.model.Shot;
 import com.jiuzhang.yeyuan.dribbbo.shot_list.ShotListAdapter;
 import com.jiuzhang.yeyuan.dribbbo.utils.ModelUtils;
 
+import java.lang.reflect.Method;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class ShotDetailActivity extends BaseActivity {
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.shot_detail_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId()) {
-//            case android.R.id.home:
-//                Intent resultIntent = new Intent();
-//                resultIntent.putExtra("position", )
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.download:
+                Toast.makeText(this, "download!", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.info:
+                Toast.makeText(this, "info!", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.url:
+                Toast.makeText(this, "URL!", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
 
     @Override
     protected Fragment newFragment() {
